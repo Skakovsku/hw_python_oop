0fe9ac492da31cf663a47362cadaa810c9a3a9a0
Евгений <Skakovsku@yandex.ru>
2021-10-16 21:58:34 +0300

    Second try sprint second

--- homework.py
+++ homework.py
@@ -1,14 +1,17 @@
 import datetime as dt
 
 
+message = 'Укажите корректный вид валюты'
+DATE_FORMAT = '%d.%m.%Y'
+
+
 class Record:
     def __init__(self, amount: float, comment: str,
                  date=None):
         self.amount = amount
         self.comment = comment
-        date_format = '%d.%m.%Y'
         if type(date) is str:
-            day_time = dt.datetime.strptime(date, date_format)
+            day_time = dt.datetime.strptime(date, DATE_FORMAT)
             self.date = day_time.date()
         elif date is None:
             self.date = dt.date.today()
@@ -26,26 +29,23 @@ class Calculator:
         self.records = []
 
     def add_record(self, record: Record) -> None:
-        """Создание записи о денежной трате"""
-        self.records = self.records + [record]
+        """Создание записи о денежной трате."""
+        self.records.append(record)
 
     def get_today_stats(self):
         """Подсчет истраченных денег и калорий за текущую дату."""
-        day_amount_count = 0
         day_now = dt.date.today()
-        for i in self.records:
-            if i.day_record() == day_now:
-                day_amount_count = day_amount_count + i.day_amount()
+        day_amount_count = sum([i.day_amount() for i in self.records
+                                if i.day_record() == day_now])
         return day_amount_count
 
     def get_week_stats(self):
         """Подсчет истраченных денег и калорий за последнюю неделю."""
-        week_amount_count = 0
         period = dt.timedelta(days=7)
         week_period = dt.date.today() - period
-        for i in self.records:
-            if dt.date.today() >= i.day_record() > week_period:
-                week_amount_count = week_amount_count + i.day_amount()
+        week_amount_count = sum([i.day_amount() for i in self.records
+                                 if dt.date.today() >= i.day_record()
+                                 > week_period])
         return week_amount_count
 
 
@@ -59,18 +59,20 @@ class CashCalculator(Calculator):
 
     def get_today_cash_remained(self, currency: str):
         """Определение оставшейся суммы на текущую дату."""
+        if currency not in self.currencies.keys():
+            raise ValueError(message)
         day_amount = self.get_today_stats()
-        rat = self.currencies
+        rate, valuta = [self.currencies[currency][1],
+                        self.currencies[currency][0]]
+        converted_rate = round((self.limit - day_amount)/rate, 2)
+        duty = -converted_rate
         if self.limit > day_amount:
-            return ('На сегодня осталось '
-                    f'{round((self.limit - day_amount)/(rat[currency][1]), 2)}'
-                    f' {rat[currency][0]}')
+            answer = f'На сегодня осталось {converted_rate} {valuta}'
         elif self.limit == day_amount:
-            return 'Денег нет, держись'
-        else:
-            return ('Денег нет, держись: твой долг - '
-                    f'{round((day_amount - self.limit)/(rat[currency][1]), 2)}'
-                    f' {rat[currency][0]}')
+            answer = 'Денег нет, держись'
+        elif self.limit < day_amount:
+            answer = f'Денег нет, держись: твой долг - {duty} {valuta}'
+        return answer
 
 
 class CaloriesCalculator(Calculator):
@@ -81,5 +83,8 @@ class CaloriesCalculator(Calculator):
         if day_amount > 0:
             return ('Сегодня можно съесть что-нибудь ещё, '
                     f'но с общей калорийностью не более {day_amount} кКал')
-        else:
-            return 'Хватит есть!'
+        return 'Хватит есть!'
+
+# Привет! Не стал писать в Slack, из-за позднего времени. С общением всё
+# удобно и нормально. Замечания понятны. Единственный момент: я раньше не
+# встречал фразу "Вынести из кода", но понял её так как сделал в своём коде.
